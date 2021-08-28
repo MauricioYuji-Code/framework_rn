ab2e1c2857812e19b8dc35aa66116718766ab1f4
MauricioYuji-Code <mauricio.nakandakari2@gmail.com>
2021-08-28 17:28:18 -0300

    upd

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,20 +9,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //Execução de um perceptron
-//        Neuron neuron1 = new Neuron();
-//        neuron1.setInput(1);
-//        Neuron neuron2 = new Neuron();
-//        neuron2.setInput(2);
-//        Neuron neuron3 = new Neuron();
-//        neuron1.addInputConnection(neuron3, 1);
-//        neuron2.addInputConnection(neuron3, 2);
-//        ArrayList<Neuron> neurons = new ArrayList<>();
-//        neurons.add(neuron1);
-//        neurons.add(neuron2);
-//        neurons.add(neuron3);
-//        Perceptron perceptron = new Perceptron(neurons);
-
         //Execução de um MLP
 //        Neuron inputNeuron1 = new Neuron();
 //        inputNeuron1.setInput(1);
